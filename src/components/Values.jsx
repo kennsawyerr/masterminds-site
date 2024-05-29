87d341dceb517bdfc95550e9cb266f483d9ac9d7
Kehinde Sawyerr <kensawyerr@gmail.com>
2024-05-29 11:39:02 +0100

    modified:   src/App.css 	modified:   src/components/Values.jsx

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import Vector from "./vector-image.jpg"
 
 const Values = () => {
+  const [selected, setSelected] =useState(false)
+
+  const handleSelect = () => {
+    setSelected(true)
+  }
   return (
     <>
       <section className="sect-pad container our-values">
@@ -8,21 +14,24 @@ const Values = () => {
         <h2>Think Beyond Lorem Ipsum</h2>
 
         <ul className="flex grid ">
-          <li>Hardwork</li>
-          <li>Quality</li>
-          <li>Integrity</li>
-          <li>Reliability</li>
+          <li onClick={handleSelect}>Hardwork</li>
+          <li onClick={handleSelect}>Quality</li>
+          <li onClick={handleSelect}>Integrity</li>
+          <li onClick={handleSelect}>Reliability</li>
         </ul>
 
-
-<div className="text-box-container flex3">
-        <div className="our-values-img-container">
-          <img src={Vector} alt="" />
-        </div>
-        <div className="our-values-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ea libero.
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti ad magni facilis quidem ducimus libero tenetur, officia omnis sapiente? Voluptatibus.
-        </div>
+        <div className="text-box-container flex3">
+          <div className="our-values-img-container">
+            <img src={Vector} alt="" />
+          </div>
+          <div
+            className={`our-values-text ${selected ? "display-value" : "hide-value "}`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+            ea libero. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Corrupti ad magni facilis quidem ducimus libero tenetur, offi cia
+            omnis sapiente? Voluptatibus.
+          </div>
         </div>
       </section>
     </>
