e5cf38663fa6a079dac4c6c5750ac346bb49613e
Kehinde Sawyerr <kensawyerr@gmail.com>
2025-03-17 22:39:09 +0100

    modified:   src/components/Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
 import Values from "./Values";
 import Landingpage from "./Landingpage";
-import Services from "./Services";
+// import Services from "./Services";
 
 const Home = () => {
   return (
     <>
       <Landingpage />
-      <Services />
       <Values />
     </>
   );
