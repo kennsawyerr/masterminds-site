47a48a2d9d934dd12416d443fea74cee52b5c5e9
Kehinde Sawyerr <kensawyerr@gmail.com>
2025-03-17 23:08:14 +0100

    modified:   src/App.css 	new file:   src/components/NValues.jsx

---
+++ src/components/NValues.jsx
@@ -0,0 +1,20 @@
+const NValues = () => {
+  return (
+    <>
+      <div>
+
+        <section className="cards">
+            <ul className="flex ">
+                <li>NAVIGATION</li>
+                <li>NAVIGATION</li>
+                <li>NAVIGATION</li>
+                <li>NAVIGATION</li>
+
+            </ul>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default NValues;
