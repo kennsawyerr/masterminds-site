61a2c8715d92d32756293c509502e5850cc45845
Kehinde Sawyerr <kensawyerr@gmail.com>
2025-03-18 23:17:50 +0100

    notfound icon

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,9 @@
+import { AlertCircle } from "lucide-react";
 const NotFound = () => {
   return (
     <>
       <div>
+        <AlertCircle/>
         <h1>404 - Page Not Found</h1>
         <p>The page you are looking for does not exist.</p>
       </div>
