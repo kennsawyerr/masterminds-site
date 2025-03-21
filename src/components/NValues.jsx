fcc3cd78391fc9166a89c340e70f6a48873a91a8
Kehinde Sawyerr <kensawyerr@gmail.com>
2025-03-21 02:42:43 +0100

    modified:   src/App.css 	modified:   src/components/NValues.jsx

--- src/components/NValues.jsx
+++ src/components/NValues.jsx
@@ -44,6 +44,24 @@ const NValues = () => {
               MARITIME
             </li>
           </ul>
+
+          <section className="comp-values-sub-h">
+            <article className="comp-values-hero">
+              <div className="textbox">
+                <h3>Connecting the dots</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Perferendis quo enim vitae earum eum velit fugiat iusto quasi
+                  quam dolorum quae perspiciatis illum, praesentium ullam
+                  debitis, facilis nam itaque labore.
+                </p>
+              </div>
+            </article>
+
+            <div className="comp-values-img">
+              <img src="marine-security.webp" alt="" />
+            </div>
+          </section>
         </section>
       </div>
     </>
